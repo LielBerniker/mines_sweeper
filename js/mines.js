a709c09b827be5ff5703f181be3e7e00d650ac56
liel berniker <lielb101995@gmail.com>
2022-09-21 11:59:18 +0300

    add negnum and build board

--- js/mines.js
+++ js/mines.js
@@ -1,14 +1,49 @@
 var gLevel = { SIZE: 4, MINES: 2 };
 var gGame = { isOn: true, shownCount: 0, markedCount: 0, secsPassed: 0 }
-var gBoard
+var gBoard = []
 function initGame() {
-
+buildBoard()
 }
-function buildBoard(board) {
-}
-function setMinesNegsCount(board)
+function buildBoard() {
+    var randLocations = randMinesLocation()
+    console.log(randLocations)
+    for (var i = 0; i < gLevel.SIZE; i++) {
+        gBoard.push([])
+        for (var j = 0; j < gLevel.SIZE; j++) {
+            var currLocation = {row: i , col: j};
+            var currCell
+            if(inLocations(currLocation,randLocations))
+            {
+                currCell = { minesAroundCount: 0, isShown: false, isMine: true, isMarked: false }
+            }
+            else{
+                currCell = { minesAroundCount: 0, isShown: false, isMine: false, isMarked: false }
+            }
+            currCell.minesAroundCount = setMinesNegsCount(i, j, randLocations)
+            gBoard[i][j] = currCell
+        }
+    }
+    console.log(gBoard)
+    return gBoard
+  }
+function setMinesNegsCount(rowIdx, colIdx, randLocations)
 {
-
+   var numNegs = 0
+    // Neighbours loop - start
+    for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
+      if (i < 0 || i >= gBoard.SIZE) continue
+      for (var j = colIdx - 1; j <= colIdx + 1; j++) {
+        if (j < 0 || j >= gBoard.SIZE) continue
+        if (i === rowIdx && j === colIdx) continue
+        var currLocation = {row: rowIdx , col:colIdx};
+        if (inLocations(currLocation,randLocations)) {
+          numNegs++
+        }
+      }
+    }
+    //Neighbours loop - end
+  
+    return numNegs
 }
 function renderBoard(board)
 {
@@ -29,4 +64,32 @@ function renderBoard(board)
  function expandShown(board, elCell, i, j)
  {
 
+ }
+
+ function randMinesLocation()
+ {
+    var randLocations = []
+    var loactionsSize = 0
+    while (loactionsSize < gLevel.MINES)
+    {
+        var currRow = getRandomInt(0,gLevel.SIZE)
+        var currCol = getRandomInt(0,gLevel.SIZE)
+        var currLocation = {row: currRow , col: currCol};
+        if(!randLocations.includes(currLocation))
+        {
+          randLocations.push(currLocation)
+          loactionsSize++
+        }
+    }
+    return randLocations
+ }
+ function inLocations(currLocation,randLocations)
+ {
+   for (let i = 0; i < randLocations.length; i++) {
+      if(currLocation.row === randLocations[i].row && currLocation.col === randLocations[i].col)
+      {
+        return true
+    }
+   }
+   return false
  }
