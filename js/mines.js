634633ac4023733f2717bf24f88b63a80ecbd9aa
liel berniker <lielb101995@gmail.com>
2022-09-21 10:31:20 +0300

    add files js

---
+++ js/mines.js
@@ -0,0 +1,32 @@
+var gLevel = { SIZE: 4, MINES: 2 };
+var gGame = { isOn: true, shownCount: 0, markedCount: 0, secsPassed: 0 }
+var gBoard
+function initGame() {
+
+}
+function buildBoard(board) {
+}
+function setMinesNegsCount(board)
+{
+
+}
+function renderBoard(board)
+{
+
+}
+ function cellClicked(elCell, i, j)
+ {
+
+ }
+ function cellMarked(elCell)
+ {
+
+ }
+ function checkGameOver()
+ {
+
+ }
+ function expandShown(board, elCell, i, j)
+ {
+
+ }
